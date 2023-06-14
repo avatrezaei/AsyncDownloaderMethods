523833195e0808075c39e9b88a5b8e2b44b0e11f
Avat <avatrezaei88@gmail.com>
2023-06-14 20:07:43 +0430

    init

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        try
+        {
+            string url = "https://example.com";
+
+            Console.WriteLine("Running DownloadStringAsync...");
+            var downloadAsyncResult = await DownloadStringAsync(url);
+            Console.WriteLine("DownloadStringAsync Result: " + downloadAsyncResult);
+
+            Console.WriteLine("\nRunning DownloadStringWithContinueWith...");
+            var downloadContinueWithResult = DownloadStringWithContinueWith(url);
+            Console.WriteLine("DownloadStringWithContinueWith Result: " + downloadContinueWithResult);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
+    }
+
+    public static async Task<string> DownloadStringAsync(string url)
+    {
+        using (var httpClient = new HttpClient())
+        {
+            try
+            {
+                string result = await httpClient.GetStringAsync(url);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+
+    public static Task<string> DownloadStringWithContinueWith(string url)
+    {
+        var httpClient = new HttpClient();
+        Task<string> downloadLink = httpClient.GetStringAsync(url);
+        return downloadLink.ContinueWith(downloadAntecedent =>
+        {
+            try
+            {
+                return downloadAntecedent.Result;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        });
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # AsyncDownloaderMethods
-Exploring #Async Programming in C#
+
+This repository contains two simple yet powerful methods to download content asynchronously from a provided URL using C#. 
+
+## Methods:
+
+1. `DownloadStringAsync`: This method downloads the content from a provided URL asynchronously.
+2. `DownloadStringWithContinueWith`: This method uses the `ContinueWith` function to download the content, which allows for additional processing after the completion of the initial task. 
+
+## Usage
+
+Replace the URL "https://example.com" in the `Main` method with the URL you want to download the content from. 
+
+Ensure you handle any exceptions that may be thrown if the URL is not reachable, if there's no internet access, or if the content at the URL is not text (e.g., an image or other binary content). 
+
+## Running the Program
+
+Compile and run the program. The console output will show the content of the URL as downloaded by both methods.
+
+Enjoy exploring the world of asynchronous programming in C#!
